d7e328d7b563ffd236fa143b8c147d9e6fb597e8
m71892016-del <m71892016@gmail.com>
2026-03-23 20:22:11 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'quiz-app-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    './images/bg-logo.jpg',
+    './fonts/Tajawal-Regular.ttf',
+    './fonts/Tajawal-Bold.ttf',
+    './audio/beep_short.ogg',
+    './audio/stadium_crowd_cheer.ogg',
+    './audio/brass_fanfare_with_timpani.ogg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then((response) => {
+            return response || fetch(event.request).catch(() => {
+                if (event.request.mode === 'navigate') {
+                    return caches.match('./index.html');
+                }
+            });
+        })
+    );
+});
